0f45a1a04bb87e5443a0e50e37de910a93b1554c
MDJ <mdj@mjolner.dk>
2026-05-21 15:33:20 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:csharp.xUnit/GildedRose/GildedRose.cs

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -2,88 +2,62 @@
 
 namespace GildedRoseKata;
 
-public class GildedRose
-{
-    IList<Item> Items;
+public class GildedRose {
+	IList<Item> Items;
 
-    public GildedRose(IList<Item> Items)
-    {
-        this.Items = Items;
-    }
+	public GildedRose(IList<Item> Items) {
+		this.Items = Items;
+	}
 
-    public void UpdateQuality()
-    {
-        for (var i = 0; i < Items.Count; i++)
-        {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
+	public void UpdateQuality() {
+		foreach (var t in Items) {
+			if (t.Name != "Aged Brie" && t.Name != "Backstage passes to a TAFKAL80ETC concert") {
+				if (t.Quality > 0) {
+					if (t.Name != "Sulfuras, Hand of Ragnaros") {
+						t.Quality = t.Quality - 1;
+					}
+				}
+			} else {
+				if (t.Quality < 50) {
+					t.Quality = t.Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+					if (t.Name == "Backstage passes to a TAFKAL80ETC concert") {
+						if (t.SellIn < 11) {
+							if (t.Quality < 50) {
+								t.Quality = t.Quality + 1;
+							}
+						}
 
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+						if (t.SellIn < 6) {
+							if (t.Quality < 50) {
+								t.Quality = t.Quality + 1;
+							}
+						}
+					}
+				}
+			}
 
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
+			if (t.Name != "Sulfuras, Hand of Ragnaros") {
+				t.SellIn = t.SellIn - 1;
+			}
 
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
-            }
-        }
-    }
+			if (t.SellIn < 0) {
+				if (t.Name != "Aged Brie") {
+					if (t.Name != "Backstage passes to a TAFKAL80ETC concert") {
+						if (t.Quality > 0) {
+							if (t.Name != "Sulfuras, Hand of Ragnaros") {
+								t.Quality = t.Quality - 1;
+							}
+						}
+					} else {
+						t.Quality = t.Quality - t.Quality;
+					}
+				} else {
+					if (t.Quality < 50) {
+						t.Quality = t.Quality + 1;
+					}
+				}
+			}
+		}
+	}
 }
