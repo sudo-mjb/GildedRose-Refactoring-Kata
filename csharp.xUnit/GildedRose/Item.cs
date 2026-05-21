40524f8937f63b34a4934e2dddad3ad04157a821
MDJ <mdj@mjolner.dk>
2026-05-21 15:38:20 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:csharp.xUnit/GildedRose/Item.cs

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -1,8 +1,58 @@
 ﻿namespace GildedRoseKata;
 
-public class Item
-{
-    public string Name { get; set; }
-    public int SellIn { get; set; }
-    public int Quality { get; set; }
+public class Item {
+	public string Name { get; set; }
+	public int SellIn { get; set; }
+	public int Quality { get; set; }
+
+	public void UpdateItem() {
+		var isAged = Name != "Aged Brie";
+		if (isAged && Name != "Backstage passes to a TAFKAL80ETC concert") {
+			if (Quality > 0) {
+				if (Name != "Sulfuras, Hand of Ragnaros") {
+					Quality = Quality - 1;
+				}
+			}
+		} else {
+			if (Quality < 50) {
+				Quality = Quality + 1;
+
+				if (Name == "Backstage passes to a TAFKAL80ETC concert") {
+					if (SellIn < 11) {
+						if (Quality < 50) {
+							Quality = Quality + 1;
+						}
+					}
+
+					if (SellIn < 6) {
+						if (Quality < 50) {
+							Quality = Quality + 1;
+						}
+					}
+				}
+			}
+		}
+
+		if (Name != "Sulfuras, Hand of Ragnaros") {
+			SellIn = SellIn - 1;
+		}
+
+		if (SellIn < 0) {
+			if (isAged) {
+				if (Name != "Backstage passes to a TAFKAL80ETC concert") {
+					if (Quality > 0) {
+						if (Name != "Sulfuras, Hand of Ragnaros") {
+							Quality = Quality - 1;
+						}
+					}
+				} else {
+					Quality = Quality - Quality;
+				}
+			} else {
+				if (Quality < 50) {
+					Quality = Quality + 1;
+				}
+			}
+		}
+	}
 }
