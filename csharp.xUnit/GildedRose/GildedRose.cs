c4e175c028933bbe5fd04c3b207cc0157517cd73
MDJ <mdj@mjolner.dk>
2026-05-21 15:39:28 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:csharp.xUnit/GildedRose/GildedRose.cs

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -10,60 +10,8 @@ public class GildedRose {
 	}
 
 	public void UpdateQuality() {
-		foreach (var item in Items)
-		{
-			UpdateItem(item);
-		}
-	}
-
-	private static void UpdateItem(Item t)
-	{
-		if (t.Name != "Aged Brie" && t.Name != "Backstage passes to a TAFKAL80ETC concert") {
-			if (t.Quality > 0) {
-				if (t.Name != "Sulfuras, Hand of Ragnaros") {
-					t.Quality = t.Quality - 1;
-				}
-			}
-		} else {
-			if (t.Quality < 50) {
-				t.Quality = t.Quality + 1;
-
-				if (t.Name == "Backstage passes to a TAFKAL80ETC concert") {
-					if (t.SellIn < 11) {
-						if (t.Quality < 50) {
-							t.Quality = t.Quality + 1;
-						}
-					}
-
-					if (t.SellIn < 6) {
-						if (t.Quality < 50) {
-							t.Quality = t.Quality + 1;
-						}
-					}
-				}
-			}
-		}
-
-		if (t.Name != "Sulfuras, Hand of Ragnaros") {
-			t.SellIn = t.SellIn - 1;
-		}
-
-		if (t.SellIn < 0) {
-			if (t.Name != "Aged Brie") {
-				if (t.Name != "Backstage passes to a TAFKAL80ETC concert") {
-					if (t.Quality > 0) {
-						if (t.Name != "Sulfuras, Hand of Ragnaros") {
-							t.Quality = t.Quality - 1;
-						}
-					}
-				} else {
-					t.Quality = t.Quality - t.Quality;
-				}
-			} else {
-				if (t.Quality < 50) {
-					t.Quality = t.Quality + 1;
-				}
-			}
+		foreach (var item in Items) {
+			item.UpdateItem();
 		}
 	}
 }
