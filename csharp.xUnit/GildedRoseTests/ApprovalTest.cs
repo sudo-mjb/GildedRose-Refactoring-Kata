860e69ca33f98d569451ce2362bd3eebb4b4042a
Søren Trudsø Mahon <soren.mahon@m10s.io>
2026-05-21 15:30:37 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:csharp.xUnit/GildedRoseTests/GildedRoseTest.cs

--- csharp.xUnit/GildedRoseTests/ApprovalTest.cs
+++ csharp.xUnit/GildedRoseTests/ApprovalTest.cs
@@ -1,38 +1,33 @@
 ﻿using GildedRoseKata;
-
 using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-
 using VerifyXunit;
-
 using Xunit;
 
 namespace GildedRoseTests;
 
-public class ApprovalTest
-{
-    [Fact]
-    public Task Foo()
-    {
-        Item[] items = { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(items);
-        app.UpdateQuality();
-        
-        return Verifier.Verify(items);
-    }
-    
-    [Fact]
-    public Task ThirtyDays()
-    {
-        var fakeoutput = new StringBuilder();
-        Console.SetOut(new StringWriter(fakeoutput));
-        Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+public class ApprovalTest {
+	
+	[Fact]
+	public Task Foo() {
+		Item[] items = { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+		GildedRose app = new GildedRose(items);
+		app.UpdateQuality();
 
-        Program.Main(new string[] { "30" });
-        var output = fakeoutput.ToString();
+		return Verifier.Verify(items);
+	}
 
-        return Verifier.Verify(output);
-    }
+	[Fact]
+	public Task ThirtyDays() {
+		var fakeoutput = new StringBuilder();
+		Console.SetOut(new StringWriter(fakeoutput));
+		Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+
+		Program.Main(new string[] { "30" });
+		var output = fakeoutput.ToString();
+
+		return Verifier.Verify(output);
+	}
 }
